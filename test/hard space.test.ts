9ecce970c4458d391cbbe8a022566d711c272547
Tomasz Śliwiński <tsliwinski.86@gmail.com>
2023-10-31 14:51:14 +0100

    Initial commit

---
+++ test/hard space.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HardSpace from '../lib/hard space-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hard space-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HardSpace.HardSpaceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
